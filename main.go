c85dabce21577cf4229d24956a5f1f8623a4d5fe
Willian Alves <wba.alves@gmail.com>
2022-06-14 22:05:20 -0300

    add small validation

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var originUnit string
@@ -17,30 +18,35 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
+  if len(os.Args) != 2 {
+    printError(errInvalidArguments)
+  }
 
-	for {
-		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+  originUnit = strings.ToUpper(os.Args[1])
 
-		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+  for {
+    fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
-		if shouldConvertAgain != "Y" {
-			fmt.Println("Good bye!")
-			break
-		}
-	}
+    fmt.Print("Would you like to convert another temperature ? (y/n) ")
+
+    if shouldConvertAgain != "Y" {
+      fmt.Println("Good bye!")
+      break
+    }
+  }
 }
 
 func printError(err error) {
-	fmt.Fprintf(os.Stderr, "error: %v\n", err)
-	os.Exit(1)
+  fmt.Fprintf(os.Stderr, "error: %v\n", err)
+  os.Exit(1)
 }
 
 func convertToCelsius(value float64) {
-	convertedValue := (value - 32) * 5 / 9
-	fmt.Printf("%v F = %.0f C\n", value, convertedValue)
+  convertedValue := (value - 32) * 5 / 9
+  fmt.Printf("%v F = %.0f C\n", value, convertedValue)
 }
 
 func convertToFahrenheit(value float64) {
-	convertedValue := (value * 9 / 5) + 32
-	fmt.Printf("%v C = %.0f F\n", value, convertedValue)
+  convertedValue := (value * 9 / 5) + 32
+  fmt.Printf("%v C = %.0f F\n", value, convertedValue)
 }
